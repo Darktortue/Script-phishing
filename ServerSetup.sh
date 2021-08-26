b00ad87acfd30661782c50ae18ff58a7977b127d
Darktortue <nbourraud@gmail.com>
2021-08-26 14:40:17 +0200

    All tests passed except gophish

--- ServerSetup.sh
+++ ServerSetup.sh
@@ -5,29 +5,14 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
-
-debian_initialize() {
-	echo "Updating and Installing Dependicies"
+debian_init() {
+	echo "Updating and installing dependicies"
 	apt update -y
 	apt upgrade -y
 	apt install git nmap golang dnsutils unzip -y
 
 	update-rc.d nfs-common disable > /dev/null 2>&1
 	update-rc.d rpcbind disable > /dev/null 2>&1
-
-	cat <<-EOF >> /etc/sysctl.conf
-	net.ipv6.conf.all.disable_ipv6 = 1
-	net.ipv6.conf.default.disable_ipv6 = 1
-	net.ipv6.conf.lo.disable_ipv6 = 1
-	net.ipv6.conf.eth0.disable_ipv6 = 1
-	net.ipv6.conf.eth1.disable_ipv6 = 1
-	net.ipv6.conf.ppp0.disable_ipv6 = 1
-	net.ipv6.conf.tun0.disable_ipv6 = 1
-	EOF
-
-	echo "IPv6 Disabled"
-
-	sysctl -p > /dev/null 2>&1
 
 	echo "Changing Hostname"
 
@@ -42,16 +27,26 @@ debian_initialize() {
 	$primary_domain
 	EOF
 
-	echo "The System will now reboot!"
+	echo
+
+	echo "The system will now reboot !"
+	echo
+	echo "3..."
+	sleep 1
+	echo "2..."
+	sleep 1
+	echo "1..."
+	sleep 1
 	reboot
 }
 
 
-reset_firewall() {
+firewall() {
 	apt install iptables-persistent -y
 
 	iptables -F
-	echo "Current iptables rules flushed"
+	echo "Current iptables rules flushed !"
+	sleep 1
 	cat <<-ENDOFRULES > /etc/iptables/rules.v4
 	*filter
 
@@ -78,13 +73,13 @@ reset_firewall() {
 	-A OUTPUT  -p tcp -m state --state NEW,ESTABLISHED --dport 80 -j ACCEPT
 	-A OUTPUT  -p tcp -m state --state NEW,ESTABLISHED --dport 443 -j ACCEPT
 
-	# Allow Mail Server Traffic outbound
+	# Allow Mail Server (IMAP, SMTP) Traffic outbound
 	-A OUTPUT  -p tcp -m state --state NEW,ESTABLISHED --dport 143 -j ACCEPT
 	-A OUTPUT  -p tcp -m state --state NEW,ESTABLISHED --dport 587 -j ACCEPT
 	-A OUTPUT  -p tcp -m state --state NEW,ESTABLISHED --dport 993 -j ACCEPT
 	-A OUTPUT  -p tcp -m state --state NEW,ESTABLISHED --dport 25 -j ACCEPT
 
-	# Allow Mail Server Traffic inbound
+	# Allow Mail Server (IMAP, SMTP) Traffic inbound
 	-A INPUT  -p tcp -m state --state NEW,ESTABLISHED --sport 143 -j ACCEPT
 	-A INPUT  -p tcp -m state --state NEW,ESTABLISHED --sport 587 -j ACCEPT
 	-A INPUT  -p tcp -m state --state NEW,ESTABLISHED --sport 993 -j ACCEPT
@@ -97,36 +92,51 @@ reset_firewall() {
 	iptables -P FORWARD DROP
 	iptables -P OUTPUT DROP
 
-	cat <<-ENDOFRULES > /etc/iptables/rules.v6
-	*filter
-
-	-A INPUT -j DROP
-	-A FORWARD -j DROP
-	-A OUTPUT -j DROP
-
-	COMMIT
-	ENDOFRULES
-
-	echo "Loading new firewall rules"
+	echo "Loading new firewall rules..."
 	iptables-restore /etc/iptables/rules.v4
-	ip6tables-restore /etc/iptables/rules.v6
+	echo "Firewall configuration done !"
 }
 
 
 
-install_ssl_Cert() {
-	echo -e "Remember to setup an A record in order for certbot to establish a connection to generate certificates..."
-    sleep 2
+ssl_cert() {
+	echo -e "Remember to setup an A record and to open port 80 or 443 in order for certbot to establish a connection to generate certificates..."
+    sleep 4
+	echo
     apt install certbot -y
-    read -p "Enter your server's domain : " -r domain
-    certbot certonly --register-unsafely-without-email --agree-tos --standalone -d "$domain"
-    ls /etc/letsencrypt/live/$domain/fullchain.pem
-    ls /etc/letsencrypt/live/$domain/privkey.pem
+	echo
+    read -rp "Enter your server's domain : " -r domain
+	echo
+    read -rp "Do you wish to configure an email address to be alerted about the renewal of the certificate ? (yY/nN) " answer
+	echo
+	case ${answer:0:1} in
+		y|Y )
+			while true
+				do
+					read -p "Enter your email address : " -r email_address
+					if [[ "$email_address" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$ ]]
+					then
+						echo "Email address $email_address is valid."
+						break
+					else
+						echo "Email address $email_address is invalid ! Please verify your input."
+					fi
+				done
+			certbot certonly --noninteractive -m "$email_address" --agree-tos --standalone -d "$domain"
+		;;
+		* )
+			certbot certonly --noninteractive --register-unsafely-without-email --agree-tos --standalone -d "$domain"
+		;;
+	esac
+
+    ls -l /etc/letsencrypt/live/"$domain"/fullchain.pem
+    ls -l /etc/letsencrypt/live/"$domain"/privkey.pem
+
+	sleep 2
 }
 
-install_postfix_dovecot() {
+install_postfix() {
 	echo "Installing Dependicies"
-	apt install dovecot-imapd dovecot-lmtpd -y
 	apt install postfix postgrey postfix-policyd-spf-python -y
 	apt install opendkim opendkim-tools -y
 	apt install opendmarc -y
@@ -135,6 +145,8 @@ install_postfix_dovecot() {
 	read -p "Enter your mail server's domain: " -r primary_domain
 	read -p "Enter IP's to allow Relay (if none just hit enter): " -r relay_ip
 	echo "Configuring Postfix"
+
+	# https://postfix.traduc.org/index.php/postconf_trie.html
 
 	cat <<-EOF > /etc/postfix/main.cf
 	smtpd_banner = \$myhostname ESMTP \$mail_name (Debian/GNU)
@@ -180,27 +192,27 @@ install_postfix_dovecot() {
 
 	echo "Configuring Opendkim"
 
-	mkdir -p "/etc/opendkim/keys/${primary_domain}"
-	cp /etc/opendkim.conf /etc/opendkim.conf.orig
+	mkdir -vp "/etc/opendkim/keys/${primary_domain}"
+	cp -v /etc/opendkim.conf /etc/opendkim.conf.orig
 
+	# http://www.opendkim.org/opendkim.conf.5.html
 	cat <<-EOF > /etc/opendkim.conf
-	domain								*
-	AutoRestart						Yes
+	domain						*
+	AutoRestart					Yes
 	AutoRestartRate				10/1h
-	Umask									0002
-	Syslog								Yes
-	SyslogSuccess					Yes
-	LogWhy								Yes
+	Umask						0002
+	Syslog						Yes
+	SyslogSuccess				Yes
+	LogWhy						Yes
 	Canonicalization			relaxed/simple
-	ExternalIgnoreList		refile:/etc/opendkim/TrustedHosts
-	InternalHosts					refile:/etc/opendkim/TrustedHosts
-	KeyFile								/etc/opendkim/keys/${primary_domain}/mail.private
-	Selector							mail
-	Mode									sv
-	PidFile								/var/run/opendkim/opendkim.pid
-	SignatureAlgorithm		rsa-sha256
-	UserID								opendkim:opendkim
-	Socket								inet:12301@localhost
+	ExternalIgnoreList			refile:/etc/opendkim/TrustedHosts
+	InternalHosts				refile:/etc/opendkim/TrustedHosts
+	KeyFile						/etc/opendkim/keys/${primary_domain}/mail.private
+	Selector					mail
+	Mode						sv
+	SignatureAlgorithm			rsa-sha256
+	UserID						opendkim:opendkim
+	Socket						inet:12301@localhost
 	EOF
 
 	cat <<-EOF > /etc/opendkim/TrustedHosts
@@ -210,15 +222,16 @@ install_postfix_dovecot() {
 	${relay_ip}
 	EOF
 
-	cd "/etc/opendkim/keys/${primary_domain}" || exit
+	cd /etc/opendkim/keys/"${primary_domain}" || exit
 	opendkim-genkey -s mail -d "${primary_domain}"
 	echo 'SOCKET="inet:12301"' >> /etc/default/opendkim
-	chown -R opendkim:opendkim /etc/opendkim
+	chown -vR opendkim:opendkim /etc/opendkim
 
 	echo "Configuring opendmarc"
 
-	chown -R opendmarc:opendmarc /var/run/opendmarc/
+	chown -vR opendmarc:opendmarc /var/run/opendmarc/
 
+	# https://manpages.debian.org/unstable/opendmarc/opendmarc.conf.5.en.html
 	cat <<-EOF > /etc/opendmarc.conf
 	AuthservID ${primary_domain}
 	PidFile /var/run/opendmarc/opendmarc.pid
@@ -238,72 +251,23 @@ install_postfix_dovecot() {
 
 	echo 'SOCKET="inet:54321"' >> /etc/default/opendmarc
 
-	echo "Configuring Dovecot"
-
-	cat <<-EOF > /etc/dovecot/dovecot.conf
-	disable_plaintext_auth = no
-	mail_privileged_group = mail
-	mail_location = mbox:~/mail:INBOX=/var/mail/%u
-
-	userdb {
-	  driver = passwd
-	}
-
-	passdb {
-	  args = %s
-	  driver = pam
-	}
-
-	protocols = " imap"
-
-	protocol imap {
-	  mail_plugins = " autocreate"
-	}
-
-	plugin {
-	  autocreate = Trash
-	  autocreate2 = Sent
-	  autosubscribe = Trash
-	  autosubscribe2 = Sent
-	}
-
-	service imap-login {
-	  inet_listener imap {
-	    port = 0
-	  }
-	  inet_listener imaps {
-	    port = 993
-	  }
-	}
-
-	service auth {
-	  unix_listener /var/spool/postfix/private/auth {
-	    group = postfix
-	    mode = 0660
-	    user = postfix
-	  }
-	}
-
-	ssl=required
-	ssl_cert = </etc/letsencrypt/live/${primary_domain}/fullchain.pem
-	ssl_key = </etc/letsencrypt/live/${primary_domain}/privkey.pem
-	EOF
-
-	read -p "What user would you like to assign to recieve email for Root: " -r user_name
+	read -p "What user would you like to assign to receive email for Root: " -r user_name
 	echo "${user_name}: root" >> /etc/aliases
 	echo "Root email assigned to ${user_name}"
 
-	echo "Restarting Services"
-	service postfix restart
-	service opendkim restart
-	service opendmarc restart
-	service dovecot restart
+	sleep 1
 
-	echo "Checking Service Status"
-	service postfix status
-	service opendkim status
-	service opendmarc status
-	service dovecot status
+	echo "Restarting services..."
+	systemctl restart postfix
+	systemctl restart opendkim
+	systemctl restart opendmarc
+
+	echo "Checking services status..."
+	systemctl --no-pager status postfix 
+	systemctl --no-pager status opendkim 
+	systemctl --no-pager status opendmarc
+
+	sleep 2
 }
 
 function add_alias(){
@@ -312,124 +276,97 @@ function add_alias(){
 	echo "${email_address}: ${user}" >> /etc/aliases
 	newaliases
 	echo "${email_address} assigned to ${user}"
+	sleep 2
 }
 
-function get_dns_entries(){
-	extip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+function dns_entries(){
+	extip=$(curl -s ipinfo.me)
 	domain=$(ls /etc/opendkim/keys/ | head -1)
-	fields=$(echo "${domain}" | tr '.' '\n' | wc -l)
 	dkimrecord=$(cut -d '"' -f 2 "/etc/opendkim/keys/${domain}/mail.txt" | tr -d "[:space:]")
 
-	if [[ $fields -eq 2 ]]; then
-		cat <<-EOF > dnsentries.txt
-		DNS Entries for ${domain}:
+	echo
+	cat <<-EOF > dnsentries.txt
+	DNS Entries for ${domain}:
 
-		====================================================================
-		OVH - Enter under Advanced DNS
+	====================================================================
 
-		Record Type: A
-		Host: @
-		Value: ${extip}
-		TTL: 5 min
+	Record Type: A
+	Host: @
+	Value: ${extip}
+	TTL: 5 min
 
-		Record Type: TXT
-		Host: @
-		Value: v=spf1 ip4:${extip} -all
-		TTL: 5 min
+	Record Type: TXT
+	Host: @
+	Value: v=spf1 ip4:${extip} -all
+	TTL: 5 min
 
-		Record Type: TXT
-		Host: mail._domainkey
-		Value: ${dkimrecord}
-		TTL: 5 min
+	If you are using a subdomain, you MUST put the sub part of your it after "domainkey". Ex : For a subdomain like toto.gouv.fr it would give : mail._domain.toto
+	Record Type: TXT
+	Host: mail._domainkey
+	Value: ${dkimrecord}
+	TTL: 5 min
 
-		Record Type: TXT
-		Host: ._dmarc
-		Value: v=DMARC1; p=reject
-		TTL: 5 min
+	Record Type: TXT
+	Host: ._dmarc
+	Value: v=DMARC1; p=reject
+	TTL: 5 min
 
-		Change Mail Settings to Custom MX and Add New Record
-		Record Type: MX
-		Host: @
-		Value: ${domain}
-		Priority: 10
-		TTL: 5 min
-		EOF
-		cat dnsentries.txt
-	else
-		prefix=$(echo "${domain}" | rev | cut -d '.' -f 3- | rev)
-		cat <<-EOF > dnsentries.txt
-		DNS Entries for ${domain}:
+	Change Mail Settings to Custom MX and Add New Record
+	Record Type: MX
+	Host: @
+	Value: ${domain}
+	Priority: 10
+	TTL: 5 min
+	EOF
 
-		====================================================================
-		OVH - Enter under Advanced DNS
+	cat dnsentries.txt
 
-		Record Type: A
-		Host: ${prefix}
-		Value: ${extip}
-		TTL: 5 min
-
-		Record Type: TXT
-		Host: ${prefix}
-		Value: v=spf1 ip4:${extip} -all
-		TTL: 5 min
-
-		Record Type: TXT
-		Host: mail._domainkey.${prefix}
-		Value: ${dkimrecord}
-		TTL: 5 min
-
-		Record Type: TXT
-		Host: ._dmarc
-		Value: v=DMARC1; p=reject
-		TTL: 5 min
-
-		Change Mail Settings to Custom MX and Add New Record
-		Record Type: MX
-		Host: ${prefix}
-		Value: ${domain}
-		Priority: 10
-		TTL: 5 min
-		EOF
-		cat dnsentries.txt
-	fi
-
+	dir=$(pwd)
+	echo
+	echo "You can find this file at this path : $dir/dnsentries.txt"
+	
+	sleep 2
 }
 
 
 function Install_GoPhish {
-	mkdir -p /opt/gophish
-	cd /opt/gophish
+	mkdir -vp /opt/gophish
+	cd /opt/gophish || exit
 	wget https://github.com/gophish/gophish/releases/download/v0.11.0/gophish-v0.11.0-linux-64bit.zip
-	unzip gophish-v0.11.0-linux-64bit.zip
+	unzip -v gophish-v0.11.0-linux-64bit.zip
 	useradd -r gophish
-	mkdir -p /var/log/gophish
-	chown gophish:gophish /var/log/gophish
-	chown -R gophish:gophish /opt/gophish
+	mkdir -vp /var/log/gophish
+	chown -v gophish:gophish /var/log/gophish
+	chown -vR gophish:gophish /opt/gophish
 	setcap 'cap_net_bind_service=+ep' gophish
     sed -i 's/"listen_url" : "127.0.0.1:3333"/"listen_url" : "0.0.0.0:3333"/g' config.json
 	read -r -p "Do you want to add an SSL certificate to your GoPhish? [y/N] " response
 	case "$response" in
 	[yY][eE][sS]|[yY])
         	 read -p "Enter your web server's domain: " -r primary_domain
-		 if [ -f "/etc/letsencrypt/live/${primary_domain}/fullchain.pem" ];then
-		 	ssl_cert="/etc/letsencrypt/live/${primary_domain}/fullchain.pem"
+		 	if [ -f "/etc/letsencrypt/live/${primary_domain}/fullchain.pem" ];then
+		 		ssl_cert="/etc/letsencrypt/live/${primary_domain}/fullchain.pem"
        		 	ssl_key="/etc/letsencrypt/live/${primary_domain}/privkey.pem"
-       		 	cp $ssl_cert ${primary_domain}.crt
-        	 	cp $ssl_key ${primary_domain}.key
+       		 	cp "$ssl_cert" "${primary_domain}".crt
+        	 	cp "$ssl_key" "${primary_domain}".key
         	 	sed -i "s/0.0.0.0:80/0.0.0.0:443/g" config.json
         	 	sed -i "s/gophish_admin.crt/${primary_domain}.crt/g" config.json
         	 	sed -i "s/gophish_admin.key/${primary_domain}.key/g" config.json
-			sed -i 's/"use_tls" : false/"use_tls" : true/g' config.json
+				sed -i 's/"use_tls" : false/"use_tls" : true/g' config.json
         	 	sed -i "s/example.crt/${primary_domain}.crt/g" config.json
         	 	sed -i "s/example.key/${primary_domain}.key/g" config.json
-		 else
-			echo "Certificate not found, use Install SSL option first"
-		 fi
-       		 ;;
-    	*)
-        	echo "GoPhish installed"
-        	;;
+		 	else
+				echo "Certificate not found, use Install SSL option first"
+		 	fi
+       		 	;;
+    		*)
+        		echo "GoPhish installed"
+        		;;
 	esac
+
+	echo "Configuration of the GoPhish systemd service..."
+	sleep 1
+
 	touch /etc/systemd/system/gophish.service
 	cat <<-EOF > /etc/systemd/system/gophish.service
 	[Unit]
@@ -447,26 +384,31 @@ function Install_GoPhish {
 	[Install]
 	WantedBy=multi-user.target
 	EOF
+
+cat /etc/systemd/system/gophish.service
+sleep 2
 }
 
 
 PS3="Server Setup Script - Pick an option: "
-options=("Debian Prep" "Install SSL" "Install Mail Server" "Add Aliases" "Get DNS Entries" "Install GoPhish")
+options=("Debian Initialization" "Firewall Configuration" "Install SSL Cert" "Install Mail Server" "Add Aliases" "Get DNS Entries" "Install GoPhish")
 select opt in "${options[@]}" "Quit"; do
 
     case "$REPLY" in
 
-	1) debian_initialize;;
+	1) debian_init;;
 
-	2) install_ssl_Cert;;
+	2) firewall;;
 
-	3) install_postfix_dovecot;;
+	3) ssl_cert;;
 
-	4) add_alias;;
+	4) install_postfix;;
 
-	5) get_dns_entries;;
+	5) add_alias;;
 
-	6) Install_GoPhish;;
+	6) dns_entries;;
+
+	7) Install_GoPhish;;
 
     $(( ${#options[@]}+1 )) ) echo "Goodbye!"; break;;
     *) echo "Invalid option. Try another one.";continue;;
